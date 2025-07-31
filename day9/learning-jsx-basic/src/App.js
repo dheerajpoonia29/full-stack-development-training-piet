9130001a48807a137ce8d5ce90ca91b6d7d72c8d
Dheeraj Bishnoi <engineerdheerajbishnoi@gmail.com>
2025-07-31 22:16:58 +0530

    day 9 done

--- day9/learning-jsx-basic/src/App.js
+++ day9/learning-jsx-basic/src/App.js
@@ -1,7 +1,19 @@
+import { VariableInJsx } from './VariableInJsx.js'
+import { ConditionInJsx } from './ConditionInJsx.js'
+import { FunctionInJsx } from './FunctionInJsx.js'
+
 function App() {
     return (
         <div>
-            <h1>react is working</h1>
+            {/* inside curly braces we write javascript expression */}
+            <p>result = {2 + 6}</p>
+
+            <VariableInJsx />
+
+            <ConditionInJsx />
+
+            <FunctionInJsx />
+
         </div>
     )
 }
---
+++ day9/learning-jsx-basic/src/VariableInJsx.js
@@ -0,0 +1,14 @@
+function VariableInJsx() {
+    let name = "dheeraj";
+    let address = "gurgram"
+
+    return (
+        <div>
+            {/* accessing variable in jsx in curly braces */}
+            <h1>my name is {name}</h1>
+            <h1>my address is {address} </h1>
+        </div>
+    );
+}
+
+export { VariableInJsx }
