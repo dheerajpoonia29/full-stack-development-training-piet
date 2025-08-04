bd59c44c483eeed9f863e374403e3884439c78a4
Dheeraj Bishnoi <engineerdheerajbishnoi@gmail.com>
2025-08-04 12:25:29 +0530

    done

--- day10/learning-react-router/src/App.js
+++ day10/learning-react-router/src/App.js
@@ -3,23 +3,25 @@ import HomePage from './HomePage'
 import Footer from './Footer'
 import ProfilePage from './ProfilePage';
 import FriendsPage from './FriendsPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div>
             <Header />
 
+
             {/* BrowserRouter component used to define routing */}
             <BrowserRouter>
                 {/* Routes component used to group route */}
                 <Routes>
-                    {/* Route component used to provide the actual route by linking path with element */}
+                    {/* Route component define path with our page component */}
                     <Route path='/home' element={<HomePage />} />
                     <Route path='/profile' element={<ProfilePage />} />
                     <Route path='/friends' element={<FriendsPage />} />
                 </Routes>
             </BrowserRouter>
+
 
             <Footer />
         </div>
